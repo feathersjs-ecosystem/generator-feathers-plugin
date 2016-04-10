2e99aa058a161d383d2555f8f959a1ba21c32804
Eric Kryski <e.kryski@gmail.com>
2016-04-09 21:44:44 -0600

    adding an example app

---
+++ app/templates/static/example/app.js
@@ -0,0 +1,21 @@
+var feathers = require('feathers');
+var rest = require('feathers-rest');
+var hooks = require('feathers-hooks');
+var bodyParser = require('body-parser');
+var errorHandler = require('feathers-errors/handler');
+var plugin = require('../lib/index');
+
+// Initialize the application
+var app = feathers()
+  .configure(rest())
+  .configure(hooks())
+  // Needed for parsing bodies (login)
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  // Configure your feathers plugin
+  .configure(plugin())
+  .use(errorHandler());
+
+app.listen(3030);
+
+console.log('Feathers app started on 127.0.0.1:3030');
